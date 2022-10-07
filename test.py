1976524ee9148fb7a5d946a746b3546ed643f56f
Spenser Reinhardt <spereinh@cisco.com>
2022-10-07 12:47:22 -0400

    oops can remove that, its in init anyway

--- test.py
+++
@@ -1,6 +0,0 @@
-#!env python3
-import logging
-logging.basicConfig(level=logging.INFO)
-from msdocs_to_dash import *
-dl = downloader.MsDownloader(downloader.DOC_SETS[1], "../doctest")
-dl.build_dash()
